6f4f2895f2aa98f8223da5331e6b771964a826b4
Florin Papa <florin.papa@intel.com>
2019-05-07 16:59:29 -0700

    Make web interface run on localhost

--- pt-visualizer/backend/sat-backend.py
+++ pt-visualizer/backend/sat-backend.py
@@ -717,4 +717,4 @@ def get_dsos_jumps(traceId, one, two):
                                 for k in result_dict]})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5005)
+    app.run(host="127.0.0.1", port=5005)
