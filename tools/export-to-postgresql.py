664c250d69229804c9b03eeda9bb040485ec0b73
Florin Papa <florin.papa@intel.com>
2019-05-13 13:08:11 -0700

    Update copyright notice

--- tools/export-to-postgresql.py
+++ tools/export-to-postgresql.py
@@ -1,5 +1,5 @@
 # export-to-postgresql.py: export perf data to a postgresql database
-# Copyright (c) 2014, Intel Corporation.
+# Copyright (c) 2014-2019, Intel Corporation.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms and conditions of the GNU General Public License,
